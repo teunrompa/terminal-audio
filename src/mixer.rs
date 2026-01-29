e1380803d69906ea3f16601a7f102bb7100e1b64
teunrompa <teunrompanew@gmail.com>
2026-01-29 01:57:59 +0100

    added creating tracks in mixer

--- src/mixer.rs
+++ src/mixer.rs
@@ -1,17 +1,23 @@
+use std::collections::HashMap;
+
 use crossterm::event::{KeyCode, KeyEvent};
 
 use crate::track::Track;
 
+//Sums all levels from track and mixes it together
+//Also used for managing track behavoir on a high level
 pub struct Mixer {
     sample_rate: f64,
-    tracks: Vec<Track>,
+    tracks: HashMap<usize, Track>,
+    selected_track: usize,
 }
 
 impl Mixer {
     pub fn new() -> Self {
         Mixer {
-            tracks: Vec::new(),
+            tracks: HashMap::new(),
             sample_rate: 0.0,
+            selected_track: 0,
         }
     }
 
@@ -19,9 +25,17 @@ impl Mixer {
         self.sample_rate = sample_rate;
     }
 
+    //Create new track and store the id in the hashmap
+    pub fn add_track(&mut self, volume: f32, name: String) {
+        let last_track_id = self.tracks.len();
+        self.tracks.insert(last_track_id, Track::new(volume, name));
+
+        self.selected_track = last_track_id;
+    }
+
     pub fn handle_keyboard_input(&mut self, key_event: KeyEvent) {
         match key_event.code {
-            KeyCode::Char('t') => todo!(), //TODO: add new track
+            KeyCode::Char('t') => self.add_track(10.0, "new track".to_string()),
             KeyCode::Char('r') => todo!(), //TODO: remove track
             _ => {}
         }
